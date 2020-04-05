69308e5c7ef6ffde29d446866c2c18aca88771ff
Chris Scholl <chris.o.scholl@gmail.com>
2020-04-05 18:32:11 -0400

    Attempt to connect with mongo in naresso

--- server/app.js
+++ server/app.js
@@ -1,18 +1,31 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import srdRouter from './routes/srd';
 import indexRouter from './routes/index';
-import usersRouter from './routes/users';
+// import usersRouter from './routes/users';
 
 const app = new express(); // eslint-disable-line new-cap
+// @TODO: set up docker container between knutz and naresso
+const mongoURI = 'mongodb://root:rootpassword@localhost:20000/admin';
+
+mongoose.connect(
+  mongoURI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Mongo connection error'));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/srd', srdRouter);
+// app.use('/users', usersRouter);
 
 export default app;
 
